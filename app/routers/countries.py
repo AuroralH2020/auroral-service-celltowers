d372cd99c47897c1726b8634941127aab2107c67
KokolM <matej.kokol@centrum.sk>
2023-03-07 15:57:20 +0100

    updated country ordering

--- app/routers/countries.py
+++ app/routers/countries.py
@@ -1,6 +1,7 @@
 from typing import List
+from sqlalchemy import desc
 from sqlalchemy.sql import text
-from .. import schemas
+from .. import schemas, models
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, status
 from ..database import get_db
@@ -8,9 +9,9 @@ from ..scripts.query_templates import groupCountries
 
 router = APIRouter()
 
+
 @router.get('/', response_model=List[schemas.CountrySchema], status_code=status.HTTP_200_OK)
 def get_all_countries(db: Session = Depends(get_db)):
     query = text(groupCountries)
     countries = db.execute(query).all()
     return countries
-
